c140b4592b0103ce9025e6cdb43718df4d7f531d
paiva2 <joaovitor.paiva145@hotmail.com>
2024-08-26 01:34:20 -0300

    [REFACTOR] ListTimeClockedUsecase hours worked on day considering interval in and out

--- clock-in-time-clock/src/main/java/org/com/clockin/timeclock/domain/usecase/timeClock/listTimeClockedUsecase/ListTimeClockedUsecase.java
+++ clock-in-time-clock/src/main/java/org/com/clockin/timeclock/domain/usecase/timeClock/listTimeClockedUsecase/ListTimeClockedUsecase.java
@@ -27,6 +27,8 @@ import java.text.ParseException;
 import java.time.Duration;
 import java.util.*;
 
+import static org.com.clockin.timeclock.domain.entity.TimeClock.Event.*;
+
 @Builder
 @Service
 @AllArgsConstructor
@@ -109,23 +111,46 @@ public class ListTimeClockedUsecase {
         return timeClockDataProvider.listAllByEmployee(employeeId, startDate, endDate);
     }
 
-    private String convertStringDuration(Duration duration) {
-        return duration.toString().replace("PT", "");
-    }
-
     private void calculateAndSetWorkHoursOfDay(LinkedHashMap<String, TimeClockListDTO> timeClockeds) {
         timeClockeds.keySet().forEach(key -> {
             TimeClockListDTO timeClockListDto = timeClockeds.get(key);
 
-            if (timeClockListDto.getTimeClockeds().size() < 2) return;
-            
-            Date inDate = timeClockListDto.getTimeClockeds().stream().map(TimeClockFilterOutput::getTimeClocked).min(Date::compareTo).get();
-            Date outDate = timeClockListDto.getTimeClockeds().stream().map(TimeClockFilterOutput::getTimeClocked).max(Date::compareTo).get();
+            if (timeClockListDto.getTimeClockeds().isEmpty()) return;
 
-            Duration hoursWorkedOnDay = Duration.between(inDate.toInstant(), outDate.toInstant());
+            Optional<TimeClockFilterOutput> timeClockedIn = findTimeClockedByEventOnList(timeClockListDto, IN);
+            if (timeClockedIn.isEmpty()) return;
 
-            timeClockListDto.setTotalHoursWorkDay(convertStringDuration(hoursWorkedOnDay));
+            Optional<TimeClockFilterOutput> timeClockedOut = findTimeClockedByEventOnList(timeClockListDto, OUT);
+
+            Long hoursWorkedOnSeconds = 0L;
+
+            Optional<TimeClockFilterOutput> timeClockedIntervalIn = findTimeClockedByEventOnList(timeClockListDto, INTERVAL_IN);
+
+            if (timeClockedIntervalIn.isPresent()) {
+                Duration timeWorkedFromInToIntervalIn = Duration.between(timeClockedIn.get().getTimeClocked().toInstant(), timeClockedIntervalIn.get().getTimeClocked().toInstant());
+
+                hoursWorkedOnSeconds += timeWorkedFromInToIntervalIn.toSeconds();
+
+                Optional<TimeClockFilterOutput> timeClockedIntervalOut = findTimeClockedByEventOnList(timeClockListDto, INTERVAL_OUT);
+
+                if (timeClockedIntervalOut.isPresent() && timeClockedOut.isPresent()) {
+                    Duration hoursWorkedFromIntervalOutToOutInSeconds = Duration.between(timeClockedIntervalOut.get().getTimeClocked().toInstant(), timeClockedOut.get().getTimeClocked().toInstant());
+                    hoursWorkedOnSeconds += hoursWorkedFromIntervalOutToOutInSeconds.toSeconds();
+                }
+            } else if (timeClockedOut.isPresent()) {
+                Duration hoursWorkedFromInToOutInSeconds = Duration.between(timeClockedIn.get().getTimeClocked().toInstant(), timeClockedOut.get().getTimeClocked().toInstant());
+                hoursWorkedOnSeconds += hoursWorkedFromInToOutInSeconds.toSeconds();
+            }
+
+            Long hoursWorked = hoursWorkedOnSeconds / 3600;
+            Long minutes = (hoursWorkedOnSeconds % 3600) / 60;
+
+            timeClockListDto.setTotalHoursWorkDay(dateHandler.buildHoursFormatString(hoursWorked, minutes));
         });
+    }
+
+    private Optional<TimeClockFilterOutput> findTimeClockedByEventOnList(TimeClockListDTO timeClockListDto, TimeClock.Event event) {
+        return timeClockListDto.getTimeClockeds().stream().filter(tc -> tc.getEventType().equals(event)).findAny();
     }
 
     private LinkedHashMap<String, TimeClockListDTO> aggregateTimeClockedByDay(List<TimeClock> timeClocks, Employee employee) {
